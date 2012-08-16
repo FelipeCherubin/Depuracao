faf82e43ae5af38b3523ea293291e5ee566408de
Felipe Cherubin <felipe_cherubin@hotmail.com>
2012-08-16 20:31:57 -0300

    adicionei um metodo para somar 1 na variavel

--- Depuracao/Program.cs
+++ Depuracao/Program.cs
@@ -6,10 +6,18 @@ namespace Depuracao
 {
     class Program
     {
+        static int Mudar(int n)
+        {
+            n = n + 1;
+            return n;
+        }
         static void Main(string[] args)
         {
             int numero = 0;
+            numero = int.Parse(Console.ReadLine());
             Console.WriteLine("o numero escolhido e: {0}", numero);
+            numero = Mudar(numero);
+            Console.WriteLine("o numero modificado e: {0}", numero);
             Console.Read();
             
 
